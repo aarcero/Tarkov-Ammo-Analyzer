9f71202933384d31b5f2369afe5f905a9a1af8e9
Alden Arce-Romero <aarcero@terpmail.umd.edu>
2026-07-11 17:56:21 -0400

    Created two functions. One for the main title and the other to load the data from the Tarkov API

---
+++ main.py
@@ -0,0 +1,13 @@
+def main_menu():
+    TITLE = "Escape from Tarkov Ammo Analyzer"
+    print(TITLE)
+    input("Press Enter to begin...")
+    print("Loading ammo data...")
+
+def load_ammo_data():
+    print("Connecting to the Tarkov API...")
+
+if __name__ == "__main__":
+    main_menu()
+    load_ammo_data()
+    
